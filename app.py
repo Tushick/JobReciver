24352d66f4566b4597f95ab4e81aa54c0c3d8737
Administrator <q>
2025-01-09 11:41:07 +0300

    Чёт работает

--- app.py
+++ app.py
@@ -5,14 +5,40 @@ from flask import Flask, render_template, request, url_for, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/json/<name_file>')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/<name_html>")
+def get_html(name_html: str):
+    return render_template(name_html)
+
+
+@app.route("/json/<name_file>")
 def get_js(name_file: str):
-    with open(name_file, 'r') as f:
-        return json.load(f)
+    path_to_file = DIRECTORY_JSON / name_file
+    if path_to_file.exists():
+        with open(path_to_file, "r") as f:
+            return json.load(f)
+    else:
+        return "Not found", 404
+
+
+# ? Функция для принятия данных из js
+@app.route("/process_data", methods=["POST"])
+def process_data():
+    if request.is_json:
+        data = request.get_json()
+        print("Received data from client:", data)
+        with open('result.json', 'w') as f:
+            json.dump(data, f)
+
+        return "received", 200
+    else:
+        return "400: Request does not have application/json header", 400
+
 
 if __name__ == "__main__":
     app.run(debug=True)
